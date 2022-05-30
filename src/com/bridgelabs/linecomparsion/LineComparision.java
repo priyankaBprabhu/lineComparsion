49167321e987fa90baefdd61ee491c3a736a9d60
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-05-30 23:08:29 +0530

    Added Line Comparission Pragram

---
+++ src/com/bridgelabs/linecomparsion/LineComparision.java
@@ -0,0 +1,8 @@
+package com.bridgelabs.linecomparsion;
+
+public class LineComparision {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+    }
+
+}
